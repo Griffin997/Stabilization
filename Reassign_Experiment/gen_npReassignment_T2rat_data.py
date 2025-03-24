1b24d8b082a1f28dd837ed6cbc6c9efeeb7a5ea4
Griffin_Surface <griffinhampton@gmail.com>
2025-03-24 11:38:05 -0500

    observing experimental params in sim

--- Reassign_Experiment/gen_npReassignment_T2rat_data.py
+++ Reassign_Experiment/gen_npReassignment_T2rat_data.py
@@ -34,11 +34,11 @@ randStart = True                  #Initial guess for parameter values in random 
 
 ######All Fixed parameters for code
 #Parameters held constant
-T11 = 600
-T12 = 1200
-c1 = 0.4
-c2 = 0.6 
-T21 = 40
+T11 = 92 #600
+T12 = 353 #1200
+c1 = 0.5 #0.4
+c2 = 0.5 #0.6 
+T21 = 31 #40
 
 
 true_params = np.array([T11, T12, c1, c2, T21])
@@ -74,10 +74,10 @@ np1_array = TI1star*np.array([-.15, -.125, -0.1, -0.075, -0.05, -0.025, 0, 0.025
 np2_array = TI2star*np.array([-.15, -.125, -0.1, -0.075, -0.05, -0.025, 0, 0.025, 0.05, 0.075, 0.1, 0.125, 0.15])//1
 
 #Adjusting the ratio of T21 and T22
-T2rat_array = np.arange(1.5, 2.51, 0.1)
+T2rat_array = np.array([41/31]) #np.arange(1.5, 2.51, 0.1)
 
 #SNR Values to Evaluate
-SNR_array = [25,35,50,75,100,150,250,500]#10**np.linspace(np.log10(25), np.log10(250), 15)
+SNR_array = [100] #[25,35,50,75,100,150,250,500]#10**np.linspace(np.log10(25), np.log10(250), 15)
 
 var_reps = 1000
 
@@ -100,7 +100,7 @@ day = date.strftime('%d')
 month = date.strftime('%B')[0:3]
 year = date.strftime('%y')
 
-num_cpus_avail = np.min([len(target_iterator),60])
+num_cpus_avail = np.min([len(target_iterator),4])
 data_path = "Reassign_Experiment/Reassign_T2rat_DATA"
 add_tag = "standard"
 data_tag = (f"reassignExp_AIC_{add_tag}_T2rat_SNRsuite_{day}{month}{year}")
